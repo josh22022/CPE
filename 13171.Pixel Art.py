f6779dea886f538b82da50fa71f4a9b497fab904
josh22022 <52907789+josh22022@users.noreply.github.com>
2022-05-07 19:47:01 +0800

    Create 13171.Pixel Art.py

---
+++ 13171.Pixel Art.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fillColor(self,m,y,c,color):
+        color_dict = {'M':[1,0,0],'Y':[0,1,0],'C':[0,0,1],'R':[1,1,0],'V':[1,0,1],'G':[0,1,1],'B':[1,1,1],'W':[0,0,0]}
+        need = [0,0,0]
+        error = 0
+        for i in range(len(color)):
+            for j in range(3):
+                need[j] += color_dict[color[i]][j]
+        m -= need[0]
+        y -= need[1]
+        c -= need[2]
+        for i in range(len(need)):
+            if m < 0 or y < 0 or c < 0:
+                error += 1
+        if error > 0:
+            return 'NO'
+        else:
+            return 'YES',m,y,c
